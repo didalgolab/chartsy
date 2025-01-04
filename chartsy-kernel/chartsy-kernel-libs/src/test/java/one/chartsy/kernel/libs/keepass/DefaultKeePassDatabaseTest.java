3f86d4d343b682e973c2a294a121ecdb17b29e85
Mariusz Bernacki <consulting@didalgo.com>
2025-01-04 01:53:45 +0100

    v0.2

--- chartsy-kernel/chartsy-kernel-libs/src/test/java/one/chartsy/kernel/libs/keepass/DefaultKeePassDatabaseTest.java
+++ chartsy-kernel/chartsy-kernel-libs/src/test/java/one/chartsy/kernel/libs/keepass/DefaultKeePassDatabaseTest.java
@@ -1,11 +1,13 @@
 package one.chartsy.kernel.libs.keepass;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultKeePassDatabaseTest {
 
+    @Disabled
     @Test
     void getDefault() {
         assertNotNull(KeePassDatabase.getDefault());
